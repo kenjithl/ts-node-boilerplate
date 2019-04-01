335dc90cb8aa0fe97c321ff4c5f2202b368d7ae6
Tan Hock Liang <kenjithl@yahoo.com>
2019-04-01 16:29:32 +0800

    Refactoring V1

--- Parts Selection.ts
+++ Parts Selection.ts
@@ -1,14 +1,16 @@
-import { question } from 'readline-sync';
+import { question } from 'readline-sync'
 
 // Laptop Builder
 // Allow users to select computer parts to create their laptop
 // Display total price with selected parts
 
 //  Step 1 : List down possible parts to select
-let screenResolution = ''
-let HDD = '' // 128 GB, 256 GB, 1TB
-let RAM = '' // 8GB, 16GB, 32 GB
-let processor = '' // i5, i7
+// let screenResolution = ''
+// let HDD = '' // 128 GB, 256 GB, 1TB
+// let RAM = '' // 8GB, 16GB, 32 GB
+// let processor = '' // i5, i7
+
+let laptop = ['', '', '', '']
 
 let totalPrice = 0
 // Step 2 : Display possible options for each part
@@ -19,83 +21,57 @@ const screenResolutionOptions = {
   '3280 x 1620': 600,
 }
 
-const hddOptions = {'128GB': 200, '256GB': 400, '1TB': 600}
-const RAMOptions = {'8GB': 200, '16GB': 500, '32GB': 800}
-const processorOptions = {'i5': 400, 'i7': 700}
-
-console.log('Screen Options')
-for (let i in screenResolutionOptions) {
-  console.log(`${i} RM ${screenResolutionOptions[i]}`)
+const hddOptions = {
+  '128GB': 200,
+  '256GB': 400,
+  '1TB': 600,
 }
-//screenResolutionOptions["1920 x 1080"] == 200
-let screenOptionIndex = question('Please select a screen option index\n')
-screenResolution = Object.keys(screenResolutionOptions)[screenOptionIndex]
-//screenResolution = screenResolutionOptions[screenOptionIndex]
 
-console.log()
-
-console.log('HDD Options')
-for (let i in hddOptions) {
-  console.log(`${i} RM ${hddOptions[i]}`)
+const RAMOptions = {
+  '8GB': 200,
+  '16GB': 500,
+  '32GB': 800,
 }
-//hddOptions["1920 x 1080"] == 200
-let hddOptionIndex = question('Please select a HDD option index\n')
-HDD = hddOptions[hddOptionIndex]
-
-console.log()
-
-console.log('RAM Options')
-for (let i in RAMOptions) {
-  console.log(`${i} RM ${RAMOptions[i]}`)
+const processorOptions = {
+  i5: 400,
+  i7: 700,
 }
-//RAMOptions["1920 x 1080"] == 200
-let ramOptionIndex = question('Please select a RAM option index\n')
-RAM = RAMOptions[ramOptionIndex]
 
-console.log()
+const options = [
+  screenResolutionOptions,
+  hddOptions,
+  RAMOptions,
+  processorOptions,
+]
 
-console.log('Processor Options')
-for (let i in processorOptions) {
-  console.log(`${i} RM ${processorOptions[i]}`)
+for (let index in options) {
+  // console.log('Part ')
+  const partsOptions = options[index]
+  for (let i in partsOptions) {
+    console.log(`${i} RM ${partsOptions[i]}`)
+  }
+  let optionIndex = question('Please select a item option index\n')
+  const selectedOption = Object.keys(partsOptions)[optionIndex]
+
+  laptop[index] = selectedOption
+
+  totalPrice = totalPrice + options[index][selectedOption]
+  console.log()
 }
-//processorOptions["1920 x 1080"] == 200
-let processorIndex = question('Please select a Processor option index\n')
-processor = processorOptions[processorIndex]
 
-console.log()
-// console.log('HDD Options')
-// for (let i in hddOptions) {
-//   console.log(`${i}. ${hddOptions[i]}`)
-// }
-// let hddOptionIndex = question('Please select a hdd option index\n')
-// HDD = hddOptions[hddOptionIndex]
-
-// console.log()
-// console.log('RAM Options')
-// for (let i in RAMOptions) {
-//   console.log(`${i}. ${RAMOptions[i]}`)
-// }
-// let ramOptionIndex = question('Please select a ram option index\n')
-// RAM = RAMOptions[ramOptionIndex]
-
-// console.log()
-// console.log('processor Options')
-// for (let i in processorOptions) {
-//   console.log(`${i}. ${processorOptions[i]}`)
-// }
-// let processorOptionIndex = question('Please select a processor option index\n')
-// processor = processorOptions[processorOptionIndex]
 //  Step 3 : List price for each selected part
 
 //  Step 4 : Select Option
 
 //  Step 5 : Display all selected options
 console.log('\nYour selected options are:')
-console.log(screenResolution)
-console.log(HDD)
-console.log(RAM)
-console.log(processor)
+// console.log(screenResolution)
+// console.log(HDD)
+// console.log(RAM)
+// console.log(processor)
+// totalPrice = screenResolutionOptions[screenResolution]
 
-totalPrice = screenResolutionOptions[screenResolution]
-
+for (let part of laptop) {
+  console.log(part)
+}
 console.log(`Your total price: ${totalPrice}`)
--- pokemon.ts
+++ pokemon.ts
@@ -1,61 +1,67 @@
-// change the pokemon names
-// change the each pokemon HP
-// battle continues until 1 pokemon faints (0 HP)
-const pokemonA: string = 'Snorlax'
-const pokemonB: string = 'Mr. Mime'
-let aHP: number = 300
-let bHP: number = 188
-const aAtk: number = 25
-const bAtk: number = 30
-const aDefend: number = 10
-const bDefend: number = 2
-const aSkilldmg: number = 4
-const bSkilldmg: number = 3
-const aDamage = (aAtk - bDefend) * aSkilldmg
-const bDamage = (bAtk - aDefend) * bSkilldmg
-//const aDamage = 50
-//const bDamage = 80
-
-console.log('You are now battling Sabrina')
-console.log('I am Sabrina, try to defeat me.')
-console.log('Sabrina sent out ' + pokemonB)
-console.log('Go, ' + pokemonA)
-
-console.log(`${pokemonA} has ${aHP} HP'`)
-console.log(`${pokemonB} has ${bHP} HP'`)
-
-const performMove = (attacker, defender, damage, defenderHP) => {
-  console.log(`${attacker} hits ${defender} for ${damage} damage`)
-  defenderHP = defenderHP - damage
-  if (defenderHP < 0) {
-    defenderHP = 0
+interface Pokemon {
+    name: string
+    hp: number
+    atk: number
+    def: number
   }
-  console.log(`${defender}'s HP is reduced to ${defenderHP} HP`)
-  return defenderHP
-}
-
-console.log('----------------------')
-let isMyTurn = true
-while (aHP > 0 && bHP > 0) {
-  if (isMyTurn) {
-    console.log('You picked Body Slam')
-    bHP = performMove(pokemonA, pokemonB, aDamage, bHP)
-  } else {
-    console.log(pokemonB + ' uses Psychic')
-    aHP = performMove(pokemonB, pokemonA, bDamage, aHP)
+  
+  const pknA: Pokemon = {
+    name: 'Snorlax',
+    hp: 100,
+    atk: 30,
+    def: 20,
   }
+  const pknB: Pokemon = {
+    name: 'Mr. Mime',
+    hp: 200,
+    atk: 25,
+    def: 25,
+  }
+  
+  console.log('You are now battling Sabrina')
+  console.log('I am Sabrina, try to defeat me.')
+  console.log('Sabrina sent out ' + pknB.name)
+  console.log('Go, ' + pknA.name)
+  
+  console.log(`${pknA.name} has ${pknA.hp} HP'`)
+  console.log(`${pknB.name} has ${pknB.hp} HP'`)
+  
+  const performMove = (attacker, defender) => {
+    const damage = attacker.atk - defender.def
+    console.log(`${attacker.name} hits ${defender.name} for ${damage} damage`)
+    defender.hp = defender.hp - damage
+  
+    if (defender.hp < 0) {
+      defender.hp = 0
+    }
+  
+    console.log(`${defender.name}'s HP is reduced to ${defender.hp} HP`)
+    return defender.hp
+  }
+  
   console.log('----------------------')
-  isMyTurn = !isMyTurn
-}
-
-if (aHP <= 0) {
-  console.log(pokemonA + ' fainted')
-  console.log('You lost, GAME OVER')
-} else {
-  console.log(pokemonB + ' fainted')
-  console.log('You won, GAME OVER')
-}
-
-// TODO: 7. Set the trainer's pokemon to 'x' for the defeated one.
-// TODO: 8. Another trainer's pokemon wil appear.
-// TODO: 9. Repeat step 4 to step 7 untill all trainer's pokemons are defeated.
+  let isMyTurn = true
+  while (pknA.hp > 0 && pknB.hp > 0) {
+    if (isMyTurn) {
+      console.log('You picked Body Slam')
+      pknB.hp = performMove(pknA, pknB)
+    } else {
+      console.log(pknB.name + ' uses Psychic')
+      pknA.hp = performMove(pknB, pknA)
+    }
+    console.log('----------------------')
+    isMyTurn = !isMyTurn
+  }
+  
+  if (pknA.hp <= 0) {
+    console.log(pknA.name + ' fainted')
+    console.log('You lost, GAME OVER')
+  } else {
+    console.log(pknB.name + ' fainted')
+    console.log('You won, GAME OVER')
+  }
+  
+  // TODO: 7. Set the trainer's pokemon to 'x' for the defeated one.
+  // TODO: 8. Another trainer's pokemon wil appear.
+  // TODO: 9. Repeat step 4 to step 7 untill all trainer's pokemons are defeated.
+  
