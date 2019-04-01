3e8b71230d5929ff6cf8b77a50875d49b9a8930f
Tan Hock Liang <kenjithl@yahoo.com>
2019-04-01 15:51:47 +0800

    V2

--- Parts Selection.ts
+++ Parts Selection.ts
@@ -1,4 +1,4 @@
-import { question } from 'readline-sync'
+import { question } from 'readline-sync';
 
 // Laptop Builder
 // Allow users to select computer parts to create their laptop
@@ -9,50 +9,82 @@ let screenResolution = ''
 let HDD = '' // 128 GB, 256 GB, 1TB
 let RAM = '' // 8GB, 16GB, 32 GB
 let processor = '' // i5, i7
-// Step 2 : Display possible options for each part
-const screenResolutionOptions = [
-  '1920 x 1080 : RM 200',
-  '2560 x 1440 : RM 400',
-  '3280 x 1620 : RM 600',
-]
-const hddOptions = ['128GB : RM 200', '256GB : RM 400', '1TB : RM 600']
-const RAMOptions = ['8GB : RM 200', '16GB : RM 500', '32GB : RM 800']
-const processorOptions = ['i5 : RM 400', 'i7 : RM 700']
-const Opt = ['Screen', 'HDD', 'RAM', 'Processor']
 
-//console.log(`${Opt[e]} Opstions`)
+let totalPrice = 0
+// Step 2 : Display possible options for each part
+// const screenResolutionOptions = ['1920 x 1080', '2560 x 1440', '3280 x 1620']
+const screenResolutionOptions = {
+  '1920 x 1080': 200,
+  '2560 x 1440': 400,
+  '3280 x 1620': 600,
+}
+
+const hddOptions = {'128GB': 200, '256GB': 400, '1TB': 600}
+const RAMOptions = {'8GB': 200, '16GB': 500, '32GB': 800}
+const processorOptions = {'i5': 400, 'i7': 700}
 
 console.log('Screen Options')
 for (let i in screenResolutionOptions) {
-    console.log(`${i}. ${screenResolutionOptions[i]}`)
+  console.log(`${i} RM ${screenResolutionOptions[i]}`)
 }
+//screenResolutionOptions["1920 x 1080"] == 200
 let screenOptionIndex = question('Please select a screen option index\n')
-screenResolution = screenResolutionOptions[screenOptionIndex]
+screenResolution = Object.keys(screenResolutionOptions)[screenOptionIndex]
+//screenResolution = screenResolutionOptions[screenOptionIndex]
 
 console.log()
+
 console.log('HDD Options')
 for (let i in hddOptions) {
-    console.log(`${i}. ${hddOptions[i]}`)
+  console.log(`${i} RM ${hddOptions[i]}`)
 }
-let hddOptionsIndex = question('Please select a HDD option index\n')
-HDD = hddOptions[hddOptionsIndex]
+//hddOptions["1920 x 1080"] == 200
+let hddOptionIndex = question('Please select a HDD option index\n')
+HDD = hddOptions[hddOptionIndex]
 
 console.log()
+
 console.log('RAM Options')
 for (let i in RAMOptions) {
-    console.log(`${i}. ${RAMOptions[i]}`)
+  console.log(`${i} RM ${RAMOptions[i]}`)
 }
-let RAMIndex = question('Please select a RAM option index\n')
-RAM = RAMOptions[RAMIndex]
+//RAMOptions["1920 x 1080"] == 200
+let ramOptionIndex = question('Please select a RAM option index\n')
+RAM = RAMOptions[ramOptionIndex]
 
 console.log()
-console.log('processor Options')
+
+console.log('Processor Options')
 for (let i in processorOptions) {
-    console.log(`${i}. ${RAMOptions[i]}`)
+  console.log(`${i} RM ${processorOptions[i]}`)
 }
+//processorOptions["1920 x 1080"] == 200
 let processorIndex = question('Please select a Processor option index\n')
 processor = processorOptions[processorIndex]
 
+console.log()
+// console.log('HDD Options')
+// for (let i in hddOptions) {
+//   console.log(`${i}. ${hddOptions[i]}`)
+// }
+// let hddOptionIndex = question('Please select a hdd option index\n')
+// HDD = hddOptions[hddOptionIndex]
+
+// console.log()
+// console.log('RAM Options')
+// for (let i in RAMOptions) {
+//   console.log(`${i}. ${RAMOptions[i]}`)
+// }
+// let ramOptionIndex = question('Please select a ram option index\n')
+// RAM = RAMOptions[ramOptionIndex]
+
+// console.log()
+// console.log('processor Options')
+// for (let i in processorOptions) {
+//   console.log(`${i}. ${processorOptions[i]}`)
+// }
+// let processorOptionIndex = question('Please select a processor option index\n')
+// processor = processorOptions[processorOptionIndex]
 //  Step 3 : List price for each selected part
 
 //  Step 4 : Select Option
@@ -63,3 +95,7 @@ console.log(screenResolution)
 console.log(HDD)
 console.log(RAM)
 console.log(processor)
+
+totalPrice = screenResolutionOptions[screenResolution]
+
+console.log(`Your total price: ${totalPrice}`)
